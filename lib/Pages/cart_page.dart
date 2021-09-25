fde9e454fde1a316feb538b2419c24b82a0896e5
Abhisheksolanki19 <abhisheksolanki19111999@gmail.com>
2021-09-25 12:56:56 +0530

    Day24 Completed

--- lib/Pages/cart_page.dart
+++ lib/Pages/cart_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/models/cart.dart';
 import 'package:flutter_catalog/widgets/theme.dart';
 import 'package:velocity_x/velocity_x.dart';
 
@@ -25,12 +26,13 @@ class _CartTotal extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final _cart = CartModel();
     return SizedBox(
       height: 200.0,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          "\$9999".text.xl4.red800.make(),
+          "\$${_cart.totalPrice}".text.xl4.red800.make(),
           30.widthBox,
           ElevatedButton(
                   style: ButtonStyle(
@@ -58,15 +60,16 @@ class _CartList extends StatefulWidget {
 }
 
 class _CartListState extends State<_CartList> {
+  final _cart = CartModel();
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: 25,
+      itemCount: _cart.items?.length,
       itemBuilder: (context, index) => ListTile(
         leading: Icon(Icons.done),
         trailing: IconButton(
             onPressed: () {}, icon: Icon(Icons.remove_circle_outline)),
-        title: "Item 1".text.make(),
+        title: _cart.items[index].name.text.make(),
       ),
     );
   }
